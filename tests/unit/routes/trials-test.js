59336baa1b218a8bd56fedbb5fd20ca39ac317d0
Brent Schooley <schooleybrent@gmail.com>
2015-09-16 14:39:33 -0400

    Naive customer display without component

---
+++ tests/unit/routes/trials-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:trials', 'Unit | Route | trials', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
